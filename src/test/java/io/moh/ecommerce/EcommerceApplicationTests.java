8c052250a769d02ea095038e547e873055a6649f
muhabdulkadir <muh.tahiropeyemi@gmail.com>
2023-04-18 05:12:47 +0100

    Fix workflow build failure

--- src/test/java/io/moh/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/io/moh/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,8 @@
 package io.moh.ecommerce;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
