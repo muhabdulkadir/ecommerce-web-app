6e258784c6641d25854147c2cf0837783d401ec4
muhabdulkadir <muh.tahiropeyemi@gmail.com>
2023-04-19 23:42:40 +0100

    Refactor category service and controller to use lambdas

--- src/main/java/io/moh/ecommerce/controller/CategoryController.java
+++ src/main/java/io/moh/ecommerce/controller/CategoryController.java
@@ -3,6 +3,7 @@ package io.moh.ecommerce.controller;
 import io.moh.ecommerce.config.ApiResponse;
 import io.moh.ecommerce.model.Category;
 import io.moh.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,35 +16,31 @@ import java.util.Objects;
 @RequestMapping("/category")
 public class CategoryController {
 
-    private final CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    @Autowired
+    private CategoryService categoryService;
 
     @PostMapping
     public ResponseEntity<ApiResponse> createCategory(@Valid @RequestBody Category category) {
         if (Objects.nonNull(categoryService.getCategoryByName(category.getCategoryName()))) {
             return new ResponseEntity<>(new ApiResponse(false, "category already exists"), HttpStatus.CONFLICT);
         }
-
         categoryService.createCategory(category);
         return new ResponseEntity<>(new ApiResponse(true, "category created successfully"), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> getCategories() {
+    public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> responseBody = categoryService.getAllCategories();
         return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
     @PutMapping("{categoryId}")
-    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @Valid @RequestBody Category category) {
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId,
+                                                      @Valid @RequestBody Category category) {
         if (Objects.nonNull(categoryService.getCategoryById(category.getId()))) {
             categoryService.updateCategory(categoryId, category);
             return new ResponseEntity<>(new ApiResponse(true, "category updated"), HttpStatus.OK);
         }
-
         return new ResponseEntity<>(new ApiResponse(false, "category does not exist"), HttpStatus.NOT_FOUND);
     }
 }
