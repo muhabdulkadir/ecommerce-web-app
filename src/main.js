c03a46e0faa57f11b0dba9a0a8db0e2c26e84acb
muhabdulkadir <muh.tahiropeyemi@gmail.com>
2023-04-20 01:32:21 +0100

    Create UI to add a category

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import swal from 'sweetalert'
 
-createApp(App).mount('#app')
+window.axios = require('axios')
+window.Swal = swal;
+
+createApp(App).use(router).mount('#app')
